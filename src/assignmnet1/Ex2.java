239d17724da3aec0dcc6107bdc8f754b28913069
Bilal Said <bilal.said@gmail.com>
2020-10-14 13:09:33 +0300

    Session 02 - OOP Review

--- src/assignmnet1/Ex2.java
+++ src/assignmnet1/Ex2.java
@@ -3,19 +3,18 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package dsfall2021;
+package assignmnet1;
 
 /**
  *
  * @author Bilal
  */
-public class DriverProgram {
+public class Ex2 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String fName = "Sam";
+        String lName = "Samra";
+        System.out.println(fName.concat(" ").concat(lName));
     }
 
 }
