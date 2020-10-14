44ee866c1e972a1fc5f91d3b5c5515468623ee22
Bilal Said <bilal.said@gmail.com>
2020-10-14 13:09:33 +0300

    Session 02 - OOP Review

--- src/dsfall2021/DriverProgram.java
+++ src/dsfall2021/DriverProgram.java
@@ -16,7 +16,6 @@ public class DriverProgram {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("dsfall2021.DriverProgram.main()");
     }
 
 }
